10bf9ab30d41978b9363e099d57cf39dbb6bd073
Barracuda Team <admin@omega8.cc>
2012-03-20 11:34:55 +0100

    Revert "Nginx should listen on all supplied IP addresses in all vhosts with ability to fallback to wildcard listen directive. Fix #2."
    This reverts commit 75f9daa631db08987baae7958fe51b8c4dd0f7e1.

--- provision/Provision/Config/cdn_disabled.tpl.php
+++ provision/Provision/Config/cdn_disabled.tpl.php
@@ -2,19 +2,10 @@
 $ip_address = !empty($ip_address) ? $ip_address : '*';
 ?>
 server {
-  limit_conn   gulag 18;
-<?php
-if ($ip_address == '*') {
-  print "  listen       {$ip_address}:{$http_port};\n";
-}
-else {
-  foreach ($server->ip_addresses as $ip) {
-    print "  listen       {$ip}:{$http_port};\n";
-  }
-}
-?>
+  listen       <?php print $ip_address . ':' . $http_port; ?>;
   server_name  <?php print implode(' ', $this->cdn); ?>;
   root         /var/www/nginx-default;
   index        index.html index.htm;
+
   ### Dont't reveal Aegir front-end URL here.
 }
