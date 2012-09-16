6bc8753da385c28c0688652046f8bb94863d7a1a
Barracuda Team <admin@omega8.cc>
2012-09-16 18:46:10 +0200

    Update vhost template.

--- Provision/Config/cdn.tpl.php
+++ Provision/Config/cdn.tpl.php
@@ -2,7 +2,7 @@
 $ip_address = !empty($ip_address) ? $ip_address : '*';
 ?>
 server {
-  limit_conn   gulag 18; # like mod_evasive - this allows max 18 simultaneous connections from one IP address
+  limit_conn   gulag 88; # like mod_evasive - this allows max 88 simultaneous connections from one IP address
 <?php
 if ($ip_address == '*') {
   print "  listen       {$ip_address}:{$http_port};\n";
@@ -28,15 +28,17 @@ else {
   ### Serve & no-log static files & images directly,
   ### without all standard drupal rewrites, php-fpm etc.
   ###
-  location ~* ^.+\.(?:css|js|htc|xml|jpe?g|gif|png|ico|bmp|svg|swf|pdf|docx?|xlsx?|tiff?|txt|rtf|cgi|bat|pl|dll|aspx?|exe|class|otf|ttf|woff)$ {
+  location ~* ^.+\.(?:css|js|htc|xml|jpe?g|gif|png|ico|bmp|svg|swf|pdf|docx?|xlsx?|pptx?|tiff?|txt|rtf|cgi|bat|pl|dll|aspx?|class|otf|ttf|woff|eot|less)$ {
     access_log  off;
     tcp_nodelay off;
     expires     30d;
+    add_header  Access-Control-Allow-Origin *;
     # allow files/images/downloads to be accessed without /sites/fqdn/
-    rewrite     ^/files/(.*)$              /sites/$host/files/$1 last;
-    rewrite     ^/images/(.*)$             /sites/$host/files/images/$1 last;
-    rewrite     ^/downloads/(.*)$          /sites/$host/files/downloads/$1 last;
-    rewrite     ^/.+/sites/.+/files/(.*)$  /sites/$host/files/$1 last;
+    rewrite     ^/files/(.*)$              /sites/$server_name/files/$1 last;
+    rewrite     ^/images/(.*)$             /sites/$server_name/files/images/$1 last;
+    rewrite     ^/downloads/(.*)$          /sites/$server_name/files/downloads/$1 last;
+    rewrite     ^/.+/sites/.+/files/(.*)$  /sites/$server_name/files/$1 last;
+    set $nocache_details "Skip";
     try_files   $uri @drupal;
   }
 
@@ -44,13 +46,16 @@ else {
   ### Serve & log bigger media/static/archive files directly,
   ### without all standard drupal rewrites, php-fpm etc.
   ###
-  location ~* ^.+\.(?:avi|mpe?g|mov|wmv|mp3|mp4|m4a|ogg|ogv|flv|wav|midi|zip|tar|t?gz|rar)$ {
+  location ~* ^.+\.(?:avi|mpe?g|mov|wmv|mp3|mp4|m4a|ogg|ogv|flv|wav|midi|zip|tar|t?gz|rar|dmg|exe)$ {
     expires     30d;
     tcp_nodelay off;
+    tcp_nopush  off;
+    add_header  Access-Control-Allow-Origin *;
     # allow files/downloads to be accessed without /sites/fqdn/
-    rewrite     ^/files/(.*)$              /sites/$host/files/$1 last;
-    rewrite     ^/downloads/(.*)$          /sites/$host/files/downloads/$1 last;
-    rewrite     ^/.+/sites/.+/files/(.*)$  /sites/$host/files/$1 last;
+    rewrite     ^/files/(.*)$              /sites/$server_name/files/$1 last;
+    rewrite     ^/downloads/(.*)$          /sites/$server_name/files/downloads/$1 last;
+    rewrite     ^/.+/sites/.+/files/(.*)$  /sites/$server_name/files/$1 last;
+    set $nocache_details "Skip";
     try_files   $uri @drupal;
   }
 
@@ -60,7 +65,7 @@ else {
   location ~* files/advagg_(?:css|js)/ {
     access_log off;
     expires    max;
-    rewrite    ^/files/advagg_(.*)/(.*)$ /sites/$host/files/advagg_$1/$2 last;
+    rewrite    ^/files/advagg_(.*)/(.*)$ /sites/$server_name/files/advagg_$1/$2 last;
     add_header ETag "";
     add_header Cache-Control "max-age=290304000, no-transform, public";
     add_header Last-Modified "Wed, 20 Jan 1988 04:20:42 GMT";
