38925bfe77c319f3b94950ff7e979e8d70a38fdb
Barracuda Team <admin@omega8.cc>
2014-09-03 21:31:27 +0200

    Remove legacy code.

--- Provision/Config/cdn_disabled.tpl.php
+++ Provision/Config/cdn_disabled.tpl.php
@@ -1,18 +1,5 @@
-<?php
-$ip_address = !empty($ip_address) ? $ip_address : '*';
-?>
 server {
-  limit_conn   limreq 555;
-<?php
-if ($ip_address == '*') {
-  print "  listen       {$ip_address}:{$http_port};\n";
-}
-else {
-  foreach ($server->ip_addresses as $ip) {
-    print "  listen       {$ip}:{$http_port};\n";
-  }
-}
-?>
+  listen       *:<?php print $http_port; ?>;
   server_name  <?php print implode(' ', $this->cdn); ?>;
   root         /var/www/nginx-default;
   index        index.html index.htm;
